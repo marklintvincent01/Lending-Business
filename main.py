bb75626b2fd51d981af1d2eb38fe68079980bbda
marklintvincentsabellano <marklintvincent.sabellano@g.msuiit.edu.ph>
2021-12-16 19:55:10 +0800

    commit cleaning up, added directories on nav buttons

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from flask.helpers import url_for
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def landingpage():
     return render_template("landingpage.html")
 
-@app.route('/loginpage')
+@app.route('/about')
+def aboutpage():
+    return render_template("about.html")
+
+@app.route('/login')
 def loginpage():
     return render_template("loginpage.html")
 
